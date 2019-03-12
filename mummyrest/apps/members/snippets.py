9e283f1b58e53bb909ddfe6fa56b84ee396ccff3
Rafael Torres <rdtr.sis@gmail.com>
2019-03-12 20:19:46 -0300

    FIX: initial investors

---
+++ mummyrest/apps/members/snippets.py
@@ -0,0 +1,18 @@
+from mummyrest.apps.utils.snippets import get_randoms
+from mummyrest.apps.utils import constants as const
+
+
+def init_member_args(member, parent, week_number):
+    values = get_randoms(3)
+    member.innocence = values[0]
+    member.experience = values[1]
+    member.charisma = values[2]
+    member.parent = parent.id
+    member.depth = parent.depth + 1
+    member.set_password(const.DEFAULT_PASWORDS)
+    if parent.map_tree:
+        member.map_tree = '{0}:{1}'.format(parent.map_tree, parent.id)
+    else:
+        member.map_tree = str(parent.id)
+    member.start_week = week_number
+    return member
---
+++ mummyrest/apps/utils/snippets.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from django.core.cache import cache
+
+
+def get_random():
+    return np.random.normal(0.5, 0.1, 1)
+
+
+def get_randoms(n=1):
+    return np.random.normal(0.5, 0.1, n)
+
+
+def init_cache():
+    cache.set('BLOK_DB', False, timeout=None)
+
+
+def db_is_avaliable():
+    return cache.get('BLOK_DB', False)
+
+
+def blocked_db():
+    return cache.set('BLOK_DB', True)
